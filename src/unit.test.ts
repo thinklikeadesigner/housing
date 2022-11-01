2b1243931dc5e7e40ba1df7bdeb520c7ee40bc95
Rebecca Burch <rebecca.burch.stu@gmail.com>
2022-10-31 23:01:19 -0500

    cleaned tests

--- src/unit.test.ts
+++ src/unit.test.ts
@@ -11,110 +11,115 @@ import { getUnitsByType, getUnitsMinMax, getAvgSqft } from './components/helpers
 
 const data = mockData;
 
-test('first object', () => {
-	expect(data[0].units[0].type).toBe('twoBdrm');
+describe('should be able to get all units by their type', () => {
+	test('studio', () => {
+		const studio = getUnitsByType(data[0], 'studio');
+		const studios = getUnitsByType(data[5], 'studio');
+		expect(studio[0].type).toBe('studio');
+		expect(studios[5].type).toBe('studio');
+	});
+	
+	test('oneBdrm', () => {
+		const oneBdrm = getUnitsByType(data[0], 'oneBdrm');
+		const oneBdrms = getUnitsByType(data[5], 'oneBdrm');
+		expect(oneBdrm[0].type).toBe('oneBdrm');
+		expect(oneBdrms[5].type).toBe('oneBdrm');
+	});
+
+	test('twoBdrm', () => {
+		const twoBdrm = getUnitsByType(data[0], 'twoBdrm');
+		const twoBdrms = getUnitsByType(data[5], 'twoBdrm');
+		expect(twoBdrm[0].type).toBe('twoBdrm');
+		expect(twoBdrms[5].type).toBe('twoBdrm');
+	});
+
+	test('threeBdrm', () => {
+		const threeBdrm = getUnitsByType(data[0], 'threeBdrm');
+		const threeBdrms = getUnitsByType(data[5], 'threeBdrm');
+		expect(threeBdrm[0].type).toBe('threeBdrm');
+		expect(threeBdrms[5].type).toBe('threeBdrm');
+	});
+
+	test('fourBdrm', () => {
+		const fourBdrm = getUnitsByType(data[0], 'fourBdrm');
+		const fourBdrms = getUnitsByType(data[5], 'fourBdrm');
+		expect(fourBdrm[0].type).toBe('fourBdrm');
+		expect(fourBdrms[5].type).toBe('fourBdrm');
+	});
+});
+	
+describe('for range of occupants', () => {
+	test('studio', () => { 
+		const studio = getUnitsByType(data[0], 'studio');
+		const studioMin = getUnitsMinMax(studio).minOcc;
+		const studioMax = getUnitsMinMax(studio).maxOcc;
+		expect(studioMin).toBe(1);
+		expect(studioMax).toBe(3);
+		expect(studioMin).toBeLessThan(studioMax);
+	});
+	test('oneBdrm', () => { 
+		const oneBdrm = getUnitsByType(data[0], 'oneBdrm');
+		const oneBdrmMin = getUnitsMinMax(oneBdrm).minOcc;
+		const oneBdrmMax = getUnitsMinMax(oneBdrm).maxOcc;
+		expect(oneBdrmMin).toBe(1);
+		expect(oneBdrmMax).toBe(5);
+		expect(oneBdrmMin).toBeLessThan(oneBdrmMax);
+	});
+	test('twoBdrm', () => { 
+		const twoBdrm = getUnitsByType(data[0], 'twoBdrm');
+		const twoBdrmMin = getUnitsMinMax(twoBdrm).minOcc;
+		const twoBdrmMax = getUnitsMinMax(twoBdrm).maxOcc;
+		expect(twoBdrmMin).toBe(1);
+		expect(twoBdrmMax).toBe(8);
+		expect(twoBdrmMin).toBeLessThan(twoBdrmMax);
+	});
+	test('threeBdrm', () => { 
+		const threeBdrm = getUnitsByType(data[0], 'threeBdrm');
+		const threeBdrmMin = getUnitsMinMax(threeBdrm).minOcc;
+		const threeBdrmMax = getUnitsMinMax(threeBdrm).maxOcc;
+		expect(threeBdrmMin).toBe(2);
+		expect(threeBdrmMax).toBe(11);
+		expect(threeBdrmMin).toBeLessThan(threeBdrmMax);
+	});
+	test('fourBdrm', () => { 
+		const fourBdrm = getUnitsByType(data[0], 'fourBdrm');
+		const fourBdrmMin = getUnitsMinMax(fourBdrm).minOcc;
+		const  fourBdrmMax = getUnitsMinMax(fourBdrm).maxOcc;
+		expect(fourBdrmMin).toBe(4);
+		expect(fourBdrmMax).toBe(12);
+		expect(fourBdrmMin).toBeLessThan(fourBdrmMax);
+	});
 });
 
 
-test('should be able to get all units by their type', () => {
-	const studio = getUnitsByType(data[0], 'studio');
-	const oneBdrm = getUnitsByType(data[0], 'oneBdrm');
-	const twoBdrm = getUnitsByType(data[0], 'twoBdrm');
-	const threeBdrm = getUnitsByType(data[0], 'threeBdrm');
-	const fourBdrm = getUnitsByType(data[0], 'fourBdrm');
-  
-	const studios = getUnitsByType(data[5], 'studio');
-	const oneBdrms = getUnitsByType(data[5], 'oneBdrm');
-	const twoBdrms = getUnitsByType(data[5], 'twoBdrm');
-	const threeBdrms = getUnitsByType(data[5], 'threeBdrm');
-	const fourBdrms = getUnitsByType(data[5], 'fourBdrm');
+describe('to get avg sq ft', () => {
+	test('to get avg sq ft', () => {
 
-
-	expect(studio[0].type).toBe('studio');
-	expect(oneBdrm[0].type).toBe('oneBdrm');
-	expect(twoBdrm[0].type).toBe('twoBdrm');
-	expect(threeBdrm[0].type).toBe('threeBdrm');
-	expect(fourBdrm[0].type).toBe('fourBdrm');
-
-	expect(studios[5].type).toBe('studio');
-	expect(oneBdrms[5].type).toBe('oneBdrm');
-	expect(twoBdrms[5].type).toBe('twoBdrm');
-	expect(threeBdrms[5].type).toBe('threeBdrm');
-	expect(fourBdrms[5].type).toBe('fourBdrm');
-});
-
-
-test('for range of occupants', () => {
-
-	const studio = getUnitsByType(data[0], 'studio');
-	const studioMin = getUnitsMinMax(studio).minOcc;
-	const studioMax = getUnitsMinMax(studio).maxOcc;
-
-	const oneBdrm = getUnitsByType(data[0], 'oneBdrm');
-
-	const  oneBdrmMin = getUnitsMinMax(oneBdrm).minOcc;
-	const oneBdrmMax = getUnitsMinMax(oneBdrm).maxOcc;
-
-	const twoBdrm = getUnitsByType(data[0], 'twoBdrm');
-
+		const studio = getUnitsByType(data[0], 'studio');
+		const studioSqFt = getAvgSqft(studio);
+		expect(studioSqFt).toBe(2580);
+	});
+	test('to get avg sq ft', () => {
 	
-	const twoBdrmMin = getUnitsMinMax(twoBdrm).minOcc;
-	const twoBdrmMax = getUnitsMinMax(twoBdrm).maxOcc;
+		const oneBdrm = getUnitsByType(data[0], 'oneBdrm');
+		const  oneBdrmSqFt = getAvgSqft(oneBdrm);
+		expect(oneBdrmSqFt).toBe(3891);
+	});
+	test('to get avg sq ft', () => {
 
-	const threeBdrm = getUnitsByType(data[0], 'threeBdrm');
+		const twoBdrm = getUnitsByType(data[0], 'twoBdrm');
+		const twoBdrmSqFt = getAvgSqft(twoBdrm);
+		expect(twoBdrmSqFt).toBe(2768);
+	});
+	test('to get avg sq ft', () => {
 
-	
-	const threeBdrmMin = getUnitsMinMax(threeBdrm).minOcc;
-	const threeBdrmMax = getUnitsMinMax(threeBdrm).maxOcc;
-	
-
-	const fourBdrm = getUnitsByType(data[0], 'fourBdrm');
-
-	const fourBdrmMin = getUnitsMinMax(fourBdrm).minOcc;
-	const  fourBdrmMax = getUnitsMinMax(fourBdrm).maxOcc;
-
-	expect(studioMin).toBe(1);
-	expect(studioMax).toBe(3);
-	expect(studioMin).toBeLessThan(studioMax);
-
-	expect(oneBdrmMin).toBe(1);
-	expect(oneBdrmMax).toBe(5);
-	expect(oneBdrmMin).toBeLessThan(oneBdrmMax);
-
-	expect(twoBdrmMin).toBe(1);
-	expect(twoBdrmMax).toBe(8);
-	expect(twoBdrmMin).toBeLessThan(twoBdrmMax);
-
-	expect(threeBdrmMin).toBe(2);
-	expect(threeBdrmMax).toBe(11);
-	expect(threeBdrmMin).toBeLessThan(threeBdrmMax);
-
-	expect(fourBdrmMin).toBe(4);
-	expect(fourBdrmMax).toBe(12);
-	expect(fourBdrmMin).toBeLessThan(fourBdrmMax);
-});	
-
-
-test('to get avg sq ft', () => {
-	const studio = getUnitsByType(data[0], 'studio');
-	const studioSqFt = getAvgSqft(studio);
-
-	const oneBdrm = getUnitsByType(data[0], 'oneBdrm');
-	const  oneBdrmSqFt = getAvgSqft(oneBdrm);
-	
-	const twoBdrm = getUnitsByType(data[0], 'twoBdrm');
-	const twoBdrmSqFt = getAvgSqft(twoBdrm);
-	
-	const threeBdrm = getUnitsByType(data[0], 'threeBdrm');
-	const threeBdrmSqFt = getAvgSqft(threeBdrm);
-	
-	const fourBdrm = getUnitsByType(data[0], 'fourBdrm');
-	const fourBdrmSqFt = getAvgSqft(fourBdrm);
-
-
-	expect(studioSqFt).toBe(2580);
-	expect(oneBdrmSqFt).toBe(3891);
-	expect(twoBdrmSqFt).toBe(2768);
-	expect(threeBdrmSqFt).toBe(2167);
-	expect(fourBdrmSqFt).toBe(3022);
+		const threeBdrm = getUnitsByType(data[0], 'threeBdrm');
+		const threeBdrmSqFt = getAvgSqft(threeBdrm);
+		expect(threeBdrmSqFt).toBe(2167);
+	});
+	test('to get avg sq ft', () => {
+		const fourBdrm = getUnitsByType(data[0], 'fourBdrm');
+		const fourBdrmSqFt = getAvgSqft(fourBdrm);
+		expect(fourBdrmSqFt).toBe(3022);
+	});
 });
