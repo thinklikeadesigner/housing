c18ceb7f9fc0182fe0b47ba5ecf5beebd4bb581d
Rebecca Burch <rebecca.burch.stu@gmail.com>
2022-11-07 16:05:24 -0600

    unused code

--- src/components/DropDownContainer/index.tsx
+++ src/components/DropDownContainer/index.tsx
@@ -1,17 +1,20 @@
 /* eslint-disable react/jsx-key */
-import React, { cloneElement, useState } from 'react';
+import React, { cloneElement, useRef, useState } from 'react';
 import './styles.css';
 
 
-const DropDownContainer = ({ title, children }: any) =>
-	<Dropdown trigger={<button className="px-4 py-2 mr-2 text-white bg-blue-500 rounded w-max hover:bg-blue-400"> {title} </button>} menu={[children]} />;
+const DropDownContainer = ({ title, children, handleOpen }: any) => {
 
 
+	return (<Dropdown trigger={<button className="px-4 py-2 mr-2 text-white bg-blue-500 rounded w-max hover:bg-blue-400"> {title} </button>} handleOpen={handleOpen} menu={[children]} />);
+
+};
 	
-const Dropdown = ({ trigger, menu }:any) => {
+const Dropdown = ({ trigger, menu }: any) => {
 	const [open, setOpen] = useState(false);
 
 	const handleOpen = () => {
+		console.log('open');
 		setOpen(!open);
 	};
 
@@ -20,20 +23,11 @@ const Dropdown = ({ trigger, menu }:any) => {
 			{cloneElement(trigger, {
 				onClick: handleOpen,
 			})}
-			{open ? (
-				<ul className="absolute z-20 w-56 p-0 list-none rounded bg-slate-200">
-					{menu.map((menuItem: any, index: any) => (
-						<li key={index} className=" menu-item">
-							{cloneElement(menuItem, {
-								onClick: () => {
-									menuItem.props.onClick();
-									setOpen(false);
-								},
-							})}
-						</li>
-					))}
-				</ul>
-			) : null}
+			{open && (
+				<div className="absolute z-20 w-56 p-0 list-none rounded bg-slate-200">
+					{menu}
+				</div>
+			)}
 		</div>
 	);
 };
