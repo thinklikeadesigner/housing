2d198651956523eb0c4134fca75d0191031a7096
Rebecca Burch <rebecca.burch.stu@gmail.com>
2022-11-07 17:52:30 -0600

    unused import

--- src/components/DropDownContainer/index.tsx
+++ src/components/DropDownContainer/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-key */
-import React, { cloneElement, useRef, useState } from 'react';
+import React, { cloneElement, useState } from 'react';
 import './styles.css';
 
 
